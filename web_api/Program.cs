ac6eb14562b667e189284b7fb9cee621eeb33f2e
fpiedrasanta <fede.piedrasanta@gmail.com>
2024-09-26 22:26:43 -0300

    2024_09_26 - fetch

--- web_api/Program.cs
+++ web_api/Program.cs
@@ -24,7 +24,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<IDAOFactory, DAOEFFactory>();
 
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("AllowAll",
+        policy => 
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
